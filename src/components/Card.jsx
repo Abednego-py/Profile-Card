f33c5431e0e4680ea69e8b9a18c0d5ed1bbd02cf
aaginam <aaginam@appzonegroup.com>
2024-02-02 19:38:08 +0100

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+export const Card = () => {
+  return (
+    <>
+      <div
+        style={{
+          border: "1px solid black",
+          width: "40%",
+          height: "60%",
+          margin: "auto",
+          position:'relative',
+          top:'20%'
+        }}
+      >
+        
+      </div>
+    </>
+  );
+};
